a4a51b889c408b7252582023c7da0c764078d066
Anurag Maiti <knoxxus@gmail.com>
2023-09-25 22:14:01 +0530

    Initial code commit-spring boot

---
+++ src/test/java/com/bits/markoth/MarkothApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bits.markoth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkothApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
