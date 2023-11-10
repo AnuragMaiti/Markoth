522e5235a372af5ddb4310c4c0c9da010c13129f
Anurag Maiti <knoxxus@gmail.com>
2023-11-10 15:22:28 +0530

    Integrated react with spring

--- src/main/java/com/bits/markoth/controller/HomeController.java
+++ src/main/java/com/bits/markoth/controller/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 @RestController
 public class HomeController {
 
@@ -11,7 +12,6 @@ public class HomeController {
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,Model model) {
         model.addAttribute("name", name);
         return "greeting";
-
     }
 
 }
